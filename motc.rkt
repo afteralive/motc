3e59609cebd6b8790afc6b825ac95d56f9889c84
liuxiaokang <floweylyu@outlook.com>
2023-08-26 10:01:53 +0800

    Add support of compiling bind source

--- motc.rkt
+++ motc.rkt
@@ -15,14 +15,20 @@
 ;;; TODO: Expand Command-line interface
 
 (define output-path (make-parameter '()))
+(define mode (make-parameter 'mot))
 
 (define target
     (command-line
         #:program "motc"
-        #:once-any
+        #:once-each
             [("-o") outpath
                 "Output file" 
                 (output-path outpath)]
+            [("-m" "--mode") outmode
+                "Compile mode(motion/bind)"
+                (mode (if (equal? outmode "bind")
+                            'bind
+                            'motion))]
         #:args (filename)
         filename))
 
@@ -31,10 +37,20 @@
 
 (let ([out (open-output-file (output-path) #:exists 'replace)])
     (src-write-to 
-        (src-generate-mot/record
-            (generate-mot/data
-                (src-parse
-                    (open-input-file
-                        target))))
+        (cond
+            [(eq? (mode) 'motion)
+                (src-generate-mot/record
+                    (generate-mot/data
+                        (src-parse
+                            (open-input-file
+                                target))))]
+            [(eq? (mode) 'bind)
+                (src-generate-bnd/record
+                    (generate-bnd/data
+                        (src-parse
+                            (open-input-file
+                                target))))])
+
+            
     out)
     (close-output-port out))
--- writer.rkt
+++ writer.rkt
@@ -3,6 +3,10 @@
 (provide
     generate-mot/data
     src-generate-mot/record
+
+    generate-bnd/data
+    src-generate-bnd/record
+
     src-write-to)
 
 (require
@@ -13,9 +17,11 @@
     "consts.rkt")
 
 (struct src-mot/long (long) #:transparent)
+(struct src-bnd/long (long) #:transparent)
 
 (struct src-data (frame value) #:transparent)
 
+;;; Motion data
 (struct src-mot/root-data src-data () #:transparent)
 (struct src-mot/step src-mot/root-data () #:transparent)
 (struct src-mot/posx src-mot/root-data () #:transparent)
@@ -29,8 +35,20 @@
 (struct src-mot/part/scalex src-mot/part-data () #:transparent)
 (struct src-mot/part/scaley src-mot/part-data () #:transparent)
 
-(struct src-mot/record (long data) #:transparent)
+;;; Bind data
 
+(struct src-bnd/data src-data ())
+(struct src-bnd/step src-bnd/data ())
+(struct src-bnd/posx src-bnd/data ())
+(struct src-bnd/posy src-bnd/data ())
+(struct src-bnd/part src-bnd/data ())
+
+;;; Records
+(struct src-record (long data) #:transparent)
+(struct src-mot/record src-record ())
+(struct src-bnd/record src-record ())
+
+;;; Raw Command operations
 (define (cmd-type cmd)
     (car cmd))
 
@@ -50,6 +68,22 @@
     (cadr cmd))
 (define (surronding-cmds cmd)
     (caddr cmd))
+
+(define (?assoc lst ele)
+    (let loop ([lft lst])
+        (if ((caar lft) ele)
+            (car lft)
+            (loop (cdr lft)))))
+
+;;; Motion attribute operations
+;;; & Generating data
+
+(define ((statement-sorter order) a b)
+    (if (equal? (src-data-frame a) (src-data-frame b))
+        (<
+            (cdr (?assoc order a))
+            (cdr (?assoc order b)))
+        (< (src-data-frame a) (src-data-frame b))))
 
 (define mot/part/attribute-mapping
     (list
@@ -81,29 +115,18 @@
                 [(with-surronding) (generate-mot/part-data (surronding-cmds cmd) (surronding-part cmd))])
     )))
 
-(define (?assoc lst ele)
-    (let loop ([lft lst])
-        (if ((caar lft) ele)
-            (car lft)
-            (loop (cdr lft)))))
-(define (mot/statement-sorter a b)
-    (define order
-        (list
-            (cons src-mot/step? 0)
-            (cons src-mot/posx? 1)
-            (cons src-mot/posy? 2)
+(define mot/statement-sorter
+    (statement-sorter (list
+                        (cons src-mot/step? 0)
+                        (cons src-mot/posx? 1)
+                        (cons src-mot/posy? 2)
 
-            (cons src-mot/part/dir? 3)
-            (cons src-mot/part/dis? 4)
-            (cons src-mot/part/angle? 5)
-            (cons src-mot/part/picture? 6)
-            (cons src-mot/part/scalex? 7)
-            (cons src-mot/part/scaley? 8)))
-    (if (equal? (src-data-frame a) (src-data-frame b))
-        (<
-            (cdr (?assoc order a))
-            (cdr (?assoc order b)))
-        (< (src-data-frame a) (src-data-frame b))))
+                        (cons src-mot/part/dir? 3)
+                        (cons src-mot/part/dis? 4)
+                        (cons src-mot/part/angle? 5)
+                        (cons src-mot/part/picture? 6)
+                        (cons src-mot/part/scalex? 7)
+                        (cons src-mot/part/scaley? 8))))
 
 (define (src-generate-mot/record data)
     (let ([long/set (filter src-mot/long? data)])
@@ -117,6 +140,47 @@
                         data)
                     mot/statement-sorter)))))
 
+;;; Bind attributes operations
+;;; & Generating datas
+
+(define bnd/attribute-mapping
+    (list
+        (cons "data_bndStep" src-bnd/step)
+        (cons "data_bndPosX" src-bnd/posx)
+        (cons "data_bndPosY" src-bnd/posy)
+        (cons "data_bndParts" src-bnd/part)))
+
+(define (generate-bnd/root-data cmd)
+    ((cdr (assoc (offset-key cmd) bnd/attribute-mapping)) (offset-frame cmd) (offset-value cmd)))
+
+(define (generate-bnd/data cmds)
+    ;;; (displayln cmds)
+    (for/list ([cmd cmds])
+        (case (cmd-type cmd)
+            [(set) (when (equal? (set-key cmd) "data_bndLong")
+                    (src-bnd/long (set-value cmd)))]
+            [(set-offset) (generate-bnd/root-data cmd)])))
+
+(define bnd/statement-sorter
+    (statement-sorter (list
+                        (cons src-bnd/step? 0)
+                        (cons src-bnd/posx? 1)
+                        (cons src-bnd/posy? 2)
+                        (cons src-bnd/part? 3))))
+
+(define (src-generate-bnd/record data)
+    (let ([long/set (filter src-bnd/long? data)])
+        (if (null? long/set)
+            (src-bnd/record '() data)
+            (src-bnd/record
+                (src-bnd/long-long (car long/set))
+                (sort
+                    (filter
+                        (lambda (e) (not (src-bnd/long? e)))
+                        data)
+                    bnd/statement-sorter)))))
+
+;;; Writters
 (define ((make-root/writer bin con) cmd out)
     (write-byte bin out)
     (write-byte (src-data-frame cmd) out)
@@ -126,6 +190,7 @@
     (write-byte (src-data-frame cmd) out)
     (write-byte (src-mot/part-data-part cmd) out)
     (write-bytes (con (src-data-value cmd)) out))
+
 (define mot/writter-mapping
     (list
         (cons src-mot/step? (make-root/writer bin-mot/step uint16->bytes))
@@ -142,18 +207,39 @@
 (define (mot/write-single cmd out)
     ((cdr (?assoc mot/writter-mapping cmd)) cmd out))
 
+(define bnd/writter-mapping
+    (list
+        (cons src-bnd/step? (make-root/writer bin-bnd/step uint16->bytes))
+        (cons src-bnd/posx? (make-root/writer bin-bnd/posx int16->bytes))
+        (cons src-bnd/posy? (make-root/writer bin-bnd/posy int16->bytes))
+        (cons src-bnd/part? (make-root/writer bin-bnd/part uint16->bytes))))
+(define (bnd/write-single cmd out)
+    ((cdr (?assoc bnd/writter-mapping cmd)) cmd out))
+
+;;; XXX: union these two similiar conditions?
 (define (src-write-to record out)
     (match record
         [(? src-mot/record?)
-                (let ([data (src-mot/record-data record)])
-                    ;;; Write motion length
-                    (unless (null? (src-mot/record-long record))
-                        (write-byte bin-bnd/long out)
-                        (write-bytes (uint16->bytes (src-mot/record-long record)) out))
-                    ;;; Write data
-                    (for ([cmd data])
-                        (mot/write-single cmd out))
-                    ;;; EOF
-                    (write-byte bin-eof out)
-                )]
+            (let ([data (src-record-data record)])
+                ;;; Write motion length
+                (unless (null? (src-record-long record))
+                    (write-byte bin-mot/long out)
+                    (write-bytes (uint16->bytes (src-record-long record)) out))
+                ;;; Write data
+                (for ([cmd data])
+                    (mot/write-single cmd out))
+                ;;; EOF
+                (write-byte bin-eof out)
+            )]
+        [(? src-bnd/record?)
+            (let ([data (src-record-data record)])
+                ;;; Write motion length
+                (unless (null? (src-record-long record))
+                    (write-byte bin-bnd/long out)
+                    (write-bytes (uint16->bytes (src-record-long record)) out))
+                ;;; Write data
+                (for ([cmd data])
+                    (bnd/write-single cmd out))
+                ;;; EOF
+                (write-byte bin-eof out))]
     ))
